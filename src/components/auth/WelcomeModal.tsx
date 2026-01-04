a4633e38995bf62117058e5fd019e015375eb66e
god-s-perfect-idiot <samarmusthafa2014@gmail.com>
2026-01-05 02:30:43 +0530

    Update WelcomeModal styles for improved readability and button size; change text colors to enhance contrast and adjust button width for better user experience.

--- src/components/auth/WelcomeModal.tsx
+++ src/components/auth/WelcomeModal.tsx
@@ -7,16 +7,16 @@ interface WelcomeModalProps {
 export default function WelcomeModal({ onGetStarted }: WelcomeModalProps) {
   return (
     <Modal className='pb-16 flex flex-col items-center'>
-      <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+      <h2 className="text-2xl font-bold text-black mb-4 text-center">
         Welcome to Spot it!
       </h2>
-      <p className="text-gray-700 text-center mb-8 leading-relaxed text-sm">
+      <p className="text-black text-center mb-8 leading-relaxed text-base">
         A private, positive, and insightful space to understand your body's unique rhythm.
       </p>
       
       <button
         onClick={onGetStarted}
-        className="w-full bg-[#ff6961] text-white font-bold text-lg py-2 px-12 max-w-[18rem] rounded-full shadow-md mt-2 mb-4"
+        className="w-full bg-[#ff6961] text-white font-bold text-lg py-2 px-12 max-w-[26rem] rounded-full shadow-md mt-2 mb-4"
       >
         Get Started
       </button>
